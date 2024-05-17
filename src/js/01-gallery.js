399679d0b8b004419ea898cede83b1875381eae1
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2024-05-17 11:47:16 +0300

    terminal commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,11 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// function sum(a, b) {
+//     return a + b;
+// };
+// function add(x, y) {
+//     return x * y;
+// };
+// export {sum, add};
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,9 @@
+// import {add,sum} from './01-gallery'
+// console.log(add(5,6));
+// console.log(sum(2,8));
 
+// или так импортировать ,усли больше 5 функций например
+
+// import *as data from './01-gallery'
+// console.log(data);
+// console.log(data.sum(5,8));
