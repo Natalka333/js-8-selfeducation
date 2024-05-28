215dc91517cd35f37133f018c386679118125f6c
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2024-05-28 12:25:35 +0000

    Deploying to gh-pages from @ Natalka333/js-8-selfeducation@a2a09462030e211b37a4c029c2cca1405321938b 🚀

---
+++ examples.10276f75.js
@@ -0,0 +1,2 @@
+!function(){localStorage.setItem("KEY",JSON.stringify([1,2,3])),console.log(localStorage.getItem("KEY"));var e=document.querySelector(".js-local"),t=document.querySelector(".js-session");e.addEventListener("click",(function(){localStorage.setItem("local","test local")})),t.addEventListener("click",(function(){sessionStorage.setItem("session","test session")})),console.log(localStorage.getItem("local"))}();
+//# sourceMappingURL=examples.10276f75.js.map
