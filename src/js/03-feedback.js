5727ca5aa0660a15f6f055617125fe678fee278d
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2024-06-03 14:23:53 +0300

    fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -9,13 +9,13 @@ form.addEventListener('input', throttle(onFormInput, 500));
 
 populateForm();
 
-
-function onFormInput(evt) {
-    const formInfo = JSON.parse(localStorage.getItem(common.KEY_FORM)) ?? {};
-    formInfo[evt.target.name] = evt.target.value;
+function onFormInput(event) {
+    let formInfo = localStorage.getItem(common.KEY_FORM);
+    // formInfo = JSON.parse(formInfo) ?? {};
+    formInfo = formInfo ? JSON.parse(formInfo) : {};
+    formInfo[event.target.name] = event.target.value;
     localStorage.setItem(common.KEY_FORM, JSON.stringify(formInfo));
-
-
+    console.log(formInfo);
 }
 
 function populateForm() {
@@ -32,15 +32,19 @@ function populateForm() {
 
 function onFormSubmit(evt) {
     evt.preventDefault();
-    const { elements: { email, message }, } = evt.currentTarget;
+    const {
+        elements: { email, message },
+    } = evt.currentTarget;
     if (email.value === '' || message.value === '') {
         return alert('fill all the fields');
     } else {
-        formData = JSON.parse(localStorage.getItem(common.KEY_FORM));
+        let formData = JSON.parse(localStorage.getItem(common.KEY_FORM));
+        console.log(formData);
+        evt.currentTarget.reset();
+        localStorage.removeItem(common.KEY_FORM);
+        formData = {};
     }
-    evt.currentTarget.reset();
-    localStorage.removeItem(common.KEY_FORM);
-formData = {};
+
 };
 
 
